054f28f5db33ef7d670f658b54629c8ec0118847
HNConan <hadrien.conan@edu.esiee.fr>
2023-05-11 14:46:41 +0200

    Update UI

---
+++ quiz-app/quiz-ui/src/services/ParticipationStorageService.js
@@ -0,0 +1,17 @@
+export default {
+  clear() {
+    window.localStorage.clear();
+  },
+  savePlayerName(playerName) {
+    window.localStorage.setItem("playerName", playerName);
+  },
+  getPlayerName() {
+    return window.localStorage.getItem("playerName");
+  },
+  saveParticipationScore(participationScore) {
+    window.localStorage.setItem("playerName", playerName);
+  },
+  getParticipationScore() {
+    return window.localStorage.getItem("participationScore");
+  }
+};
