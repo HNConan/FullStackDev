ca60d9a15cf5c050070ca096bfbc4c4d2d069d6f
HNConan <hadrien.conan@edu.esiee.fr>
2023-05-11 11:49:36 +0200

    Commit UI

---
+++ quiz-app/quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>NewQuizz page</h1>
+  </div>
+  <button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
